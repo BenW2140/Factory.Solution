2492f50685ad9b6c02ce02e7f3ab4f2722bc30f5
Ben White <bwhite2140@outlook.com>
2020-08-07 11:09:20 -0700

    add engineer create route and view

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -1,13 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
 using System.Linq;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Factory.Controllers
 {
   public class EngineersController : Controller
   {
     public readonly FactoryContext _db;
-
     public EngineersController(FactoryContext db)
     {
       _db = db;
@@ -15,6 +15,22 @@ namespace Factory.Controllers
     public ActionResult Index()
     {
       return View(_db.Engineers.ToList());
+    }
+    public ActionResult Create()
+    {
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+      return View();
+    }
+    [HttpPost]
+    public ActionResult Create(Engineer engineer, int MachineId)
+    {
+      _db.Engineers.Add(engineer);
+      if (MachineId != 0)
+      {
+        _db.EngineerMachine.Add(new EngineerMachine() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
     }
   }
 }
